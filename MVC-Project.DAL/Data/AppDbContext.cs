21fe35316e651906a18b78e6b31bd1dbec88acf6
Omar Ebrahim Mohamed Khalil <200012464@must.edu.eg>
2024-09-05 22:38:08 +0300

    Creating Table and DB context

---
+++ MVC-Project.DAL/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.DAL.Data
+{
+    internal class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; DataBase=C42_MVC; Trusted_Connection=True; MultipleActiveResultSets = True"); 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Fluent Apis
+            modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
